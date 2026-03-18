8496161504d19cf5aa09f87346efd4d930462267
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2026-03-18 15:08:55 +0200

    Player movement

---
+++ Player/Scripts/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	
+	direction = direction.normalized()
+	velocity = direction * SPEED
+	
+	move_and_slide()

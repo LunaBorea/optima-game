763389a1ea36e1718dd14959465162af959f903c
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2026-04-09 00:55:18 +0300

    Added map, camera follows player

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 175.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
